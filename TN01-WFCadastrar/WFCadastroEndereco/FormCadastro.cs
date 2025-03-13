a8433817aa65fd0d5189590fd14c995fbee76d04
DANIEL GUIMARAES DANTAS DE ALMEIDA <daniel.gdalmeida@senacsp.edu.br>
2025-03-12 21:13:14 -0300

    Adicionar arquivos de projeto.

---
+++ TN01-WFCadastrar/WFCadastroEndereco/FormCadastro.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCadastroEndereco
+{
+    public partial class FormCadastro : Form
+    {
+        public FormCadastro()
+        {
+            InitializeComponent();
+
+        }
+        public void Alerta(string mensagem = "")
+        {
+            MessageBox.Show(mensagem, "Alerta",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        public void Erro(string mensagem = "")
+        {
+            MessageBox.Show(mensagem, "Erro",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public void Sucesso(string mensagem = "")
+        {
+            MessageBox.Show(mensagem, "Sucesso",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Endereco end = new Endereco();
+
+            if (string.IsNullOrEmpty(mtbCep.Text))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtLogradouro.Text))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtNumero.Text))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtBairro.Text))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtCidade.Text))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+            if (string.IsNullOrEmpty(cbxUf.SelectedItem?.ToString()))
+            {
+                Erro("Campo Vazio");
+                return;
+            }
+
+
+
+
+
+        }
+
+        private void chkSemNumero_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSemNumero.Checked == true)
+            {
+                txtNumero.Enabled = false;
+            }
+            else
+            {
+                txtNumero.Enabled = true;
+            }
+        }
+    }
+}
